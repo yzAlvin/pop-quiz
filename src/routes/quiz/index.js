0e0a8c7c9b59c41b940dbc44915e6d25503244fa
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-08-20 17:14:37 +1000

    refactor menu to separate component and generalize sets

--- src/routes/quiz/index.js
+++ src/routes/quiz/index.js
@@ -3,12 +3,7 @@ import { useState } from "preact/hooks";
 import { hiragana } from './hiragana'
 import { katakana } from './katakana';
 
-const s = {
-  "hiragana": hiragana,
-  "katakana": katakana
-}
-
-const Question = ({ questionSet }) => {
+const Questions = ({ questionSet }) => {
   const [n, setN] = useState(0)
   const [answer, setAnswer] = useState("");
   const [correct, setCorrect] = useState(0);
@@ -43,14 +38,15 @@ const Question = ({ questionSet }) => {
   )
 }
 
-const Quiz = () => {
-  const [questionSet, setQuestionSet] = useState({})
-  const [checked, setChecked] = useState({"hiragana": false, "katakana": false})
-  const [quizStart, setQuizStart] = useState(false)
-  
+String.prototype.toTitleCase = function() {
+  return this.charAt(0).toUpperCase() + this.substring(1)
+}
+
+const QuizMenu = ({setQuestionSet, checked, setChecked, setQuizStart, sets}) => {
+  const setOptions = Object.keys(sets)
   const handleSubmit = (e) => {
     const selectedSets = Object.keys(checked).filter(k => checked[k])
-    const set = selectedSets.map(selectedSet => s[selectedSet])
+    const set = selectedSets.map(selectedSet => sets[selectedSet])
     setQuestionSet(Object.assign(...set))
     setQuizStart(true)
     e.preventDefault();
@@ -62,28 +58,38 @@ const Quiz = () => {
   }
 
   return (
+    <form onSubmit={handleSubmit}>
+      {setOptions.map(set => 
+        <>
+          <input 
+            type="checkbox"
+            id={set}
+            checked={checked[set]}
+            onChange={handleOnChange}
+          />
+          <label>{set.toTitleCase()}</label>
+          <br/>
+        </>
+      )}
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+const Quiz = () => {
+  const sets = {
+    "hiragana": hiragana,
+    "katakana": katakana
+  }
+  const [questionSet, setQuestionSet] = useState({})
+  const [checked, setChecked] = useState(Object.keys(sets).reduce((allSets, set) => ({...allSets, [set]: false}), {}))
+  const [quizStart, setQuizStart] = useState(false)
+
+  return (
     <div class={style.quiz}>
       <h1>Quiz</h1>
-      {!quizStart && <form onSubmit={handleSubmit}>
-        <input 
-          type="checkbox"
-          id="hiragana"
-          checked={checked["hiragana"]}
-          onChange={handleOnChange}
-        />
-        <label>Hiragana</label>
-        <br/>
-        <input
-          type="checkbox"
-          id="katakana"
-          checked={checked["katakana"]}
-          onChange={handleOnChange}
-        />
-        <label>Katakana</label>
-        <br/>
-        <input type="submit" value="Submit" />
-      </form>}
-      {quizStart && <Question questionSet={questionSet} />}
+      {!quizStart && <QuizMenu setQuestionSet={setQuestionSet} checked={checked} setChecked={setChecked} setQuizStart={setQuizStart} sets={sets} />}
+      {quizStart && <Questions questionSet={questionSet} />}
     </div>
   );
 }
