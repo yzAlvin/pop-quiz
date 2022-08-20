1df013cc083a63c1f1026e1fd12635d67a40a97f
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-08-20 16:03:12 +1000

    refactor to use dictionary over array of object for question set

--- src/routes/quiz/hiragana.js
+++ src/routes/quiz/hiragana.js
@@ -1,26 +1,7 @@
-const a = {
-    answer: "a",
-    question: "あ"
-};
-
-const i = {
-    answer: "i",
-    question: "い"
-};
-
-const u = {
-    answer: "u",
-    question: "う"
-};
-
-const e = {
-    answer: "e",
-    question: "え"
-};
-
-const o = {
-    answer: "o",
-    question: "お"
-};
-
-export const hiragana = [a, i, u, e, o]
+export const hiragana = {
+    "あ": "a",
+    "い": "i",
+    "う": "u",
+    "え": "e",
+    "お": "o",
+}
--- src/routes/quiz/index.js
+++ src/routes/quiz/index.js
@@ -1,22 +1,24 @@
 import style from './style.css';
 import { useState } from "preact/hooks";
 import { hiragana } from './hiragana'
+import { katakana } from './katakana';
 
 const Question = ({ questionSet }) => {
   const [n, setN] = useState(0)
   const [answer, setAnswer] = useState("");
   const [correct, setCorrect] = useState(0);
   const [quizState, setQuizState] = useState("Not End")
+  const questions = Object.keys(questionSet)
 
   const nextQuestion = () => {
-    if (answer === questionSet[n].answer) setCorrect(correct + 1);
-    setN((n + 1) % questionSet.length);
+    if (answer === questionSet[questions[n]]) setCorrect(correct + 1);
+    setN(n + 1);
     setAnswer("");
   }
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      if (n + 1 === questionSet.length) {
+      if (n + 1 === questions.length) {
         setQuizState("End")
       }
       nextQuestion()
@@ -26,20 +28,21 @@ const Question = ({ questionSet }) => {
   return (
     <div class={style.question}>
       {quizState === "End" ?
-        <h3>{correct}/{questionSet.length}</h3> :
+        <h3>{correct}/{questions.length}</h3> :
         <>
-          <h3>{questionSet[n].question}</h3>
+          <h3>{questions[n]}</h3>
           <input type="text" value={answer} onInput={e => setAnswer(e.target.value)} onKeyDown={handleKeyDown} />
         </>
       }
     </div>
   )
-};
+}
 
 const Quiz = () => (
   <div class={style.quiz}>
     <h1>Quiz</h1>
     <Question questionSet={hiragana} />
+    <Question questionSet={katakana} />
   </div>
 );
 
