4af08e71eead57a03b8db8b636c5405fc4e7e9bc
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-08-20 19:32:47 +1000

    add all basic hisagana

--- src/routes/quiz/hiragana.js
+++ src/routes/quiz/hiragana.js
@@ -1,7 +1,87 @@
+export const a = {
+  あ: "a",
+  い: "i",
+  う: "u",
+  え: "e",
+  お: "o",
+};
+
+export const k = {
+  か: "ka",
+  き: "ki",
+  く: "ku",
+  け: "ke",
+  こ: "ko",
+};
+
+export const s = {
+  さ: "sa",
+  し: "si",
+  す: "su",
+  せ: "se",
+  そ: "so",
+};
+
+export const t = {
+  た: "ta",
+  ち: "chi",
+  つ: "tsu",
+  て: "te",
+  と: "to",
+};
+
+export const n = {
+  な: "na",
+  に: "ni",
+  ぬ: "nu",
+  ね: "ne",
+  の: "no",
+};
+
+export const h = {
+  は: "ha",
+  ひ: "hi",
+  ふ: "hu",
+  へ: "he",
+  ほ: "ho",
+};
+
+export const m = {
+  ま: "ma",
+  み: "mi",
+  む: "mu",
+  め: "me",
+  も: "mo",
+};
+
+export const y = {
+  や: "ya",
+  ゆ: "yu",
+  よ: "yo",
+};
+
+export const r = {
+  ら: "ra",
+  り: "ri",
+  る: "ru",
+  れ: "re",
+  ろ: "ro",
+};
+
+export const w = {
+  わ: "wa",
+  を: "wo",
+};
+
 export const hiragana = {
-    "あ": "a",
-    "い": "i",
-    "う": "u",
-    "え": "e",
-    "お": "o",
-}
+  ...a,
+  ...k,
+  ...s,
+  ...t,
+  ...n,
+  ...h,
+  ...m,
+  ...y,
+  ...r,
+  ...w,
+};
